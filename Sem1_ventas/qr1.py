59aa72411fadad6651b3aee76b68e0c8d8d30fd9
Sebastian <sebastianayala201@gmail.com>
2023-10-30 00:39:11 -0500

    ordenando carpetas

---
+++ Sem1_ventas/qr1.py
@@ -0,0 +1,11 @@
+import qrcode
+
+input = "https://www.tiktok.com/foryou?is_copy_url=1&is_from_webapp=v1"
+
+qr = qrcode.QRCode(version=1,box_size=10,border=5)
+
+qr.add_data(input)
+qr.make(fit=True)
+
+img = qr.make_image(fill='black',back_color='green')
+img.save('qr1.png')
